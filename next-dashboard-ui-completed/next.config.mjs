81ec698ac91d3f96bff02957e389c3ce1ad68d37
ngoctoan189 <ngoctoan1892002@gmail.com>
2025-06-12 17:07:33 +0700

    started

--- next-dashboard-ui-completed/next.config.mjs
+++ next-dashboard-ui-completed/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [{ hostname: "images.pexels.com" }],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
