e9737971cb89f6560aef1e349d5f70078979373c
ngoctoan189 <ngoctoan1892002@gmail.com>
2025-06-20 17:29:47 +0700

    add list folder

--- dashboard-ui/src/app/(dashboard)/admin/page.tsx
+++ dashboard-ui/src/app/(dashboard)/admin/page.tsx
@@ -34,7 +34,7 @@ const AdminPage = () => {
         </div>
       </div>
       {/*RIGHT*/}
-      <div className="w-full lg:w-1/3 flex flex">
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
         {/* CALENDAR */}
           <EventCalendar />
           <Announcements />
---
+++ dashboard-ui/src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,19 @@
+const TeacherListPage = () => {
+  return (
+    <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+        {/* TOP */}
+        <div className="">
+            <h1>All teachers</h1>
+            <div className=""></div>
+        </div>
+        {/* LIST */}
+        <div className="">
+
+        </div>
+        {/* PAGINATION */}
+        <div className=""></div>
+    </div>
+  )
+}
+
+export default TeacherListPage
