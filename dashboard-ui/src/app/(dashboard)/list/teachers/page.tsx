e65e1fe363e8cd8feeb8935dbb8ddaa6a49a5b29
ngoctoan189 <ngoctoan1892002@gmail.com>
2025-06-24 14:40:14 +0700

    feat: integrate Prisma for database management and add teacher listing functionality
    - Added Prisma client as a dependency and configured it for database interactions.
    - Created a new migration for initializing the database schema with tables for Admin, Student, Teacher, Parent, Grade, Class, Subject, Lesson, Exam, Assignment, Result, Attendance, Event, and Announcement.
    - Implemented a seeding script to populate the database with initial data for testing.
    - Updated the TeacherListPage component to fetch teacher data from the database, including related subjects and classes.
    - Refactored the teacher data structure to include subjects and classes as arrays of objects.
    - Added a default avatar image for teachers without a profile picture.
    - Introduced a settings file to manage pagination constants.

--- dashboard-ui/src/app/(dashboard)/list/teachers/page.tsx
+++ dashboard-ui/src/app/(dashboard)/list/teachers/page.tsx
@@ -3,20 +3,13 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEMS_PER_PAGE } from "@/lib/settings";
+import { Class, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Teacher = {
-  id: number;
-  teacherId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
-  address: string;
-};
+type TeacherList = Teacher & {subjects: Subject[]} & {classes: Class[]};
 
 const columns = [
   {
@@ -53,16 +46,14 @@ const columns = [
     accessor: "action",
   },
 ];
-
-const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
+const renderRow = (item: TeacherList) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">
         <Image
-          src={item.photo}
+          src={item.img || "/noAvatar.png"}
           alt=""
           width={40}
           height={40}
@@ -73,9 +64,9 @@ const TeacherListPage = () => {
           <p className="text-xs text-gray-500">{item?.email}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.username}</td>
+      <td className="hidden md:table-cell">{item.subjects.map(subject=>subject.name).join(",")}</td>
+      <td className="hidden md:table-cell">{item.classes.map(classItem => classItem.name).join(",")}</td>
       <td className="hidden md:table-cell">{item.phone}</td>
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
@@ -94,7 +85,26 @@ const TeacherListPage = () => {
         </div>
       </td>
     </tr>
-  );
+);
+const TeacherListPage = async ({searchParams}:{searchParams:{[key:string]:string | undefined};}) => {
+
+  const {page, ...queryParams} = searchParams
+
+  const p = page ? parseInt(page) : 1;
+
+
+  const data = await prisma.teacher.findMany({
+    include:{
+      subjects: true,
+      classes: true,
+    },
+    take:ITEMS_PER_PAGE,
+    skip: ITEMS_PER_PAGE * (p - 1),
+  })
+
+  const count = await prisma.teacher.count
+
+  // console.log(data)
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -120,7 +130,7 @@ const TeacherListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
       <Pagination />
     </div>
