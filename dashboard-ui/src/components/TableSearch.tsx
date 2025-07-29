7c629aa2886491de1f182212be23d1b9a5264632
toanhn <ngoctoan1892002@gmail.com>
2025-07-29 14:48:55 +0700

    update TableSearch component to provide default values for props

--- dashboard-ui/src/components/TableSearch.tsx
+++ dashboard-ui/src/components/TableSearch.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-const TableSearch = ({ value, onChange }: { value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) => {
+const TableSearch = ({ value = '', onChange = () => {}, }: { value?: string; onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void }) => {
   return (
     <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
       <Image src="/search.png" alt="" width={14} height={14} />
